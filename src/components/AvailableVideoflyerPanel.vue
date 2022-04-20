9f552f8e3857ecbcb49c24a6673759842ff64f92
Andreas Mueller <andreas.mueller@yaga.eu>
2022-04-20 18:38:18 +0200

    add assignment notes

--- src/components/AvailableVideoflyerPanel.vue
+++ src/components/AvailableVideoflyerPanel.vue
@@ -10,6 +10,7 @@
             <th scope="col">{{ $t("allDay") }}</th>
             <th scope="col">{{ $t("from") }}</th>
             <th scope="col">{{ $t("to") }}</th>
+            <th scope="col">{{ $t("note") }}</th>
           </tr>
         </thead>
         <tbody>
@@ -31,6 +32,16 @@
             </td>
             <td>{{ videoflyer.from }}</td>
             <td>{{ videoflyer.to }}</td>
+            <td>
+              <v-tooltip v-if="videoflyer.note" bottom>
+                <template #activator="{ on, attrs }">
+                  <v-icon color="primary" dark v-bind="attrs" v-on="on">
+                    mdi-note
+                  </v-icon>
+                </template>
+                <span>{{ videoflyer.note }}</span>
+              </v-tooltip>
+            </td>
           </tr>
         </tbody></v-simple-table
       ></v-card
