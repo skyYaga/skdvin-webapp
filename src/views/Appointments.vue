ab506a2eecd78ba0e613a49f32ec8fe10098a68f
Andreas Mueller <andreas.mueller@yaga.eu>
2022-05-24 18:54:08 +0200

    eslint allow console error

--- src/views/Appointments.vue
+++ src/views/Appointments.vue
@@ -109,14 +109,14 @@ export default {
             query: { date },
           })
           .catch((err) => {
-            // Ignore the vuex err regarding  navigating to the page they are already on.
+            // Ignore the vuex err regarding navigating to the page they are already on.
             if (
               err.name !== "NavigationDuplicated" &&
               !err.message.includes(
                 "Avoided redundant navigation to current location"
               )
             ) {
-              // But print any other errors to the console
+              // eslint-disable-next-line
               console.error(err);
             }
           });
